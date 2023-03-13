9e09bbc4e56313f5bb4c68d31e323c1017873106
Anser Waseem <hafiz.anser.waseem@gmail.com>
2023-03-13 12:38:53 +0500

    first commit

---
+++ src/store/items/reducer.js
@@ -0,0 +1,12 @@
+let id = 1;
+
+export const initialItems = [
+  { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
+  { uuid: id++, name: 'Vegan Ham Sandwich', price: 12, quantity: 1 }
+];
+
+export const reducer = (state = initialItems, action) => {
+  return state;
+};
+
+export default reducer;
